cc4744ccf5e230d75fc3368d7a5bba75cf6ac316
leonardoXoliveira <xleonardo.x.oliveirax@gmail.com>
2025-02-18 20:46:25 -0300

    chore: upgrade Tailwind CSS to v4 and update related configurations

--- src/components/ui/button.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-import { Button } from './button'
-
-test('SHOULD render a button', () => {
-  render(<Button>Click me</Button>)
-  expect(screen.getByText('Click me')).toBeInTheDocument()
-})
