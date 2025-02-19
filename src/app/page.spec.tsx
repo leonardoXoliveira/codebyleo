10901b3a3a81f1da8270eaa765888bd6187ddffb
leonardoXoliveira <xleonardo.x.oliveirax@gmail.com>
2025-02-19 10:14:43 -0300

    fix: correct typo in home page text and add tests

---
+++ src/app/page.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it } from 'vitest'
+import Page from './page'
+
+describe('Page', () => {
+  it('SHOULD render', () => {
+    render(<Page />)
+    expect(screen.getByText('New Hello World')).toBeInTheDocument()
+  })
+})
